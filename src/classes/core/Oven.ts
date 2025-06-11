9d7cd198da13bfebd90d37853162ad58669e0a44
bendiegamer <bendiereal@gmail.com>
2025-06-11 16:24:33 +0000

    I give up D:

--- src/classes/core/Oven.ts
+++ src/classes/core/Oven.ts
@@ -23,134 +23,89 @@
 
 import OMod from "./Mod";
 
-interface ExampleItemConfig {
-  itemName?: string;
-  itemTexture?: string;
+class OItem {
+  const creativeMiscTab: any = ModAPI.reflect.getClassById(
+    "net.minecraft.creativetab.CreativeTabs"
+  ).staticVariables.tabMisc;
+  const itemClass: any = ModAPI.reflect.getClassById("net.minecraft.item.Item");
+  const itemSuper: Function = ModAPI.reflect.getSuper(
+    itemClass,
+    (x: any[]) => x.length === 1
+  );
+
+  function nmi_ItemExample(this: any): void {
+    itemSuper(this);
+    this.$setCreativeTab(creativeMiscTab);
+  }
+
+  ModAPI.reflect.prototypeStack(itemClass, nmi_ItemExample);
+
+  nmi_ItemExample.prototype.$onItemRightClick = function (
+    $itemstack: any,
+    $world: any,
+    $player: any
+  ): any {
+    return $itemstack;
+  };
+
+  function internal_reg(): any {
+    const example_item: any = new nmi_ItemExample().$setUnlocalizedName(
+      ModAPI.util.str("exampleitem")
+    );
+    itemClass.staticMethods.registerItem.method(
+      ModAPI.keygen.item("exampleitem"),
+      ModAPI.util.str("exampleitem"),
+      example_item
+    );
+    ModAPI.items["exampleitem"] = example_item;
+
+    return example_item;
+  }
+
+  if (ModAPI.items) {
+    return internal_reg();
+  } else {
+    ModAPI.addEventListener("bootstrap", internal_reg);
+  }
 }
 
-export class OItem {
-  creativeMiscTab: any;
-  itemClass: any;
-  itemSuper: any;
-  exampleItemInstance: any;
-  itemName: string;
-  itemTexture?: string;
+ModAPI.dedicatedServer.appendCode(ExampleItem);
+const example_item: any = ExampleItem();
 
-  constructor(config: ExampleItemConfig = {}) {
-    this.itemName = config.itemName || "exampleitem";
-    this.itemTexture = config.itemTexture;
+ModAPI.addEventListener("lib:asyncsink", async () => {
+  ModAPI.addEventListener("lib:asyncsink:registeritems", (renderItem: any) => {
+    renderItem.registerItem(example_item, ModAPI.util.str("exampleitem"));
+  });
 
-    this.creativeMiscTab = ModAPI.reflect.getClassById(
-      "net.minecraft.creativetab.CreativeTabs"
-    ).staticVariables.tabMisc;
-    this.itemClass = ModAPI.reflect.getClassById("net.minecraft.item.Item");
-    this.itemSuper = ModAPI.reflect.getSuper(
-      this.itemClass,
-      (x: any[]) => x.length === 1
-    );
+  AsyncSink.L10N.set("item.exampleitem.name", "Example Item");
 
-    this.defineItemClass();
-    this.registerItem();
-  }
+  AsyncSink.setFile(
+    "resourcepacks/AsyncSinkLib/assets/minecraft/models/item/exampleitem.json",
+    JSON.stringify({
+      parent: "builtin/generated",
+      textures: {
+        layer0: "items/exampleitem",
+      },
+      display: {
+        thirdperson: {
+          rotation: [-90, 0, 0],
+          translation: [0, 1, -3],
+          scale: [0.55, 0.55, 0.55],
+        },
+        firstperson: {
+          rotation: [0, -135, 25],
+          translation: [0, 4, 2],
+          scale: [1.7, 1.7, 1.7],
+        },
+      },
+    })
+  );
 
-  private defineItemClass() {
-    const creativeMiscTab = this.creativeMiscTab;
-    const itemSuper = this.itemSuper;
-    console.log("adding item...");
-    function nmi_ItemExample(this: any) {
-      itemSuper(this);
-      this.$setCreativeTab(creativeMiscTab);
-    }
-
-    ModAPI.reflect.prototypeStack(this.itemClass, nmi_ItemExample);
-
-    nmi_ItemExample.prototype.$onItemRightClick = function (
-      $itemstack: any,
-      $world: any,
-      $player: any
-    ) {
-      return $itemstack;
-    };
-
-    this.nmi_ItemExample = nmi_ItemExample as any;
-  }
-
-  private nmi_ItemExample: any;
-
-  private internal_reg() {
-    this.exampleItemInstance = new this.nmi_ItemExample();
-    this.exampleItemInstance.$setUnlocalizedName(
-      ModAPI.util.str(this.itemName)
-    );
-
-    this.itemClass.staticMethods.registerItem.method(
-      ModAPI.keygen.item(this.itemName),
-      ModAPI.util.str(this.itemName),
-      this.exampleItemInstance
-    );
-
-    ModAPI.items[this.itemName] = this.exampleItemInstance;
-  }
-
-  private registerItem() {
-    if (ModAPI.items) {
-      this.internal_reg();
-    } else {
-      ModAPI.addEventListener("bootstrap", () => this.internal_reg());
-    }
-  }
-
-  getItemInstance() {
-    return this.exampleItemInstance;
-  }
-
-  async setupAsyncSink() {
-    if (!this.exampleItemInstance) return;
-
-    ModAPI.addEventListener("lib:asyncsink", async () => {
-      ModAPI.addEventListener(
-        "lib:asyncsink:registeritems",
-        (renderItem: any) => {
-          renderItem.registerItem(
-            this.exampleItemInstance,
-            ModAPI.util.str(this.itemName)
-          );
-        }
-      );
-
-      AsyncSink.L10N.set(`item.${this.itemName}.name`, "Example Item");
-
-      AsyncSink.setFile(
-        `resourcepacks/AsyncSinkLib/assets/minecraft/models/item/${this.itemName}.json`,
-        JSON.stringify({
-          parent: "builtin/generated",
-          textures: {
-            layer0: `items/${this.itemName}`,
-          },
-          display: {
-            thirdperson: {
-              rotation: [-90, 0, 0],
-              translation: [0, 1, -3],
-              scale: [0.55, 0.55, 0.55],
-            },
-            firstperson: {
-              rotation: [0, -135, 25],
-              translation: [0, 4, 2],
-              scale: [1.7, 1.7, 1.7],
-            },
-          },
-        })
-      );
-
-      if (this.itemTexture) {
-        AsyncSink.setFile(
-          `resourcepacks/AsyncSinkLib/assets/minecraft/textures/items/${this.itemName}.png`,
-          await (await fetch(this.itemTexture)).arrayBuffer()
-        );
-      }
-    });
-  }
-}
+  AsyncSink.setFile(
+    "resourcepacks/AsyncSinkLib/assets/minecraft/textures/items/exampleitem.png",
+    await (await fetch(itemTexture)).arrayBuffer()
+  );
+});
 
 // example:
 // new OItem("exampleitem", "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAAXNSR0IArs4c6QAAAKZJREFUOE9j/P//PxMDBIBoEP6HREOl4PLIciA2AyPIgMcM//7KgvWSDJjBBpx9/+YvJzc3Sbq12DhB6sEGsJ19/+YnmQawYhigzc7FcPXnN4KugbqAHWQAy9n3b34T4wJkw6EGYLqAoNVQBWS5ANlwZBfAvUCs/0EGkW0AzBKqGoCSDgh5A80F2KMRpAgfAKUT6kcjsfEPUycmKMQgy8AETkgUZWcAS3CPIf4oSPsAAAAASUVORK5CYII=");
