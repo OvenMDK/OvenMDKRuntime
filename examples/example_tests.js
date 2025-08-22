52c5b0cb28b42983216d4c90f824d3924dac803a
stpv22 <oswaldfink12@gmail.com>
2025-08-22 12:19:07 -0700

    Planning of full Entity Stuff

--- examples/example_tests.js
+++ examples/example_tests.js
@@ -1,7 +1,38 @@
 
+var example = new OEntity('Example', 'example');
 
-var example = new OEntity('example', '1', '', '')
+//this is meant as an exmaple to show that this can be done through both functions and direct property access
+//none of this has been actually implemented yet
+
+//base64 png image
+example.setTexture('');
+example.entityTexture = '';
+
+//json model
+example.setModel('{}');
+example.entityModel = '{}';
+
+//spawn egg (base, spot colors)
+example.createSpawnEgg(0x5e3e2d, 0x269166);
+
+//item needed, delay in ticks
+example.makeBreedable('wheat', 2000);
+example.setBreedingCooldown(2000); //ticks
+example.breedable = true;
+example.breedItem = 'wheat';
+example.breedingCooldown = 2000; //ticks
+
+//loot tables will be added later
+//item, min amount, max amount
+example.addDropItem('feather', 0, 2);
+example.dropItem = 'feather';
+
+example.setHealth(20);
+example.maxHealth = 20;
+
+//wip
+var sounds = example.createSoundObject('random');
+sounds.addSound('1', 'soundfile');
+example.setMainSound(sounds); //whis will be changed later
 
 example.registerOEntity();
-
-example.createSpawnegg('')
